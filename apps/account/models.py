28f71cdf316791eb57806d09de1ecee151b2a88c
pnabiniw <pnabiniw@gmail.com>
2024-05-31 20:45:50 +0545

    custom manager

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,7 +1,37 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.hashers import make_password
 from apps.commons.models import DateTimeModel
+
+
+class CustomUserManager(UserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_verified", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -12,6 +42,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManager()
 
 
 class UserProfile(DateTimeModel):
@@ -25,3 +57,10 @@ class UserProfile(DateTimeModel):
     def __str__(self):
         return f"Profile of {self.user.email}"
 
+
+class UserAccountActivation(DateTimeModel):
+    email = models.EmailField(max_length=50)
+    key = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Key of {self.email}"
