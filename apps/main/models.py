c1cda8215a40f2fbe0a0031d7f813878f49541d4
pnabiniw <pnabiniw@gmail.com>
2024-06-11 19:33:02 +0545

    home page

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from apps.commons.models import DateTimeModel
 
-# Create your models here.
+User = get_user_model()
+
+
+class Category(DateTimeModel):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Job(DateTimeModel):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    location = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category_jobs")
+    apply_before = models.DateTimeField()
+    experience_required = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+APPLIED = "Applied"
+SCREENING = "Screening"
+DECLINED = "Declined"
+CALL_FOR_INTERVIEW = "Call For Interview"
+REJECTED = "Rejected"
+SELECTED = "Selected"
+APPLICATION_STATUS = (
+    (APPLIED, APPLIED),
+    (SCREENING, SCREENING),
+    (DECLINED, DECLINED),
+    (CALL_FOR_INTERVIEW, CALL_FOR_INTERVIEW),
+    (REJECTED, REJECTED),
+    (SELECTED, SELECTED)
+)
+
+
+class JobApplication(DateTimeModel):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name="job_applications")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_applications")
+    status = models.CharField(choices=APPLICATION_STATUS, max_length=20)
+    interview_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Application by {self.user.email} for {self.job.title}"
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Job
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
     template_name = "main/home.html"
+    queryset = Job.objects.all().order_by("-created_at")
+    paginate_by = 6
